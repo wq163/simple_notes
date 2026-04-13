da2f7322351743b7fee2e9cacd7b930f9a165745
jenwang <jenwang@thunderwind.cn>
2026-04-13 14:48:13 +0800

    apk signing

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -15,8 +15,22 @@ android {
         versionName = "1.0"
     }
 
-    buildTypes {
+    signingConfigs {
         release {
+            // 从环境变量读取，如果读取不到则不报错（方便本地开发）
+            storeFile = file(System.getenv("SIGNING_STORE_FILE") ?: "debug.keystore")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+    buildTypes {
+        debug {
+            // 让 Debug 版本也使用这个签名配置，这样签名就固定了
+            signingConfig = signingConfigs.release
+        }
+        release {
+            signingConfig = signingConfigs.release
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
